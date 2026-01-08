38f235e199e155766c33e761c1afb4cf990388e2
mrdsx <andre0.castlegames@gmail.com>
2026-01-08 18:46:20 +0300

    remove text selection in glass button

--- src/components/glass-button.tsx
+++ src/components/glass-button.tsx
@@ -6,10 +6,14 @@ type GlassButtonProps = React.ComponentProps<"div"> &
   VariantProps<typeof glassButtonVariants>;
 
 const glassButtonVariants = cva(
-  "focus-visible:border-ring focus-visible:ring-ring/50 inline-flex w-fit cursor-pointer items-center gap-1 border leading-none transition-colors outline-none hover:bg-gray-800 focus-visible:ring-1",
+  "focus-visible:border-ring focus-visible:ring-ring/50 inline-flex w-fit cursor-pointer items-center gap-1 border leading-none transition-colors outline-none hover:bg-gray-800 focus-visible:ring-1 select-none",
   {
     variants: {
-      size: { default: "gap-2 p-2 [&>svg]:w-6", sm: "p-1.25", lg: "p-4" },
+      size: {
+        default: "gap-2 p-2 [&>svg]:w-6",
+        sm: "p-1.25",
+        lg: "p-4 select-text cursor-default",
+      },
     },
     defaultVariants: { size: "default" },
   },
